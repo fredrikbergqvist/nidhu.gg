1e0c7659b029be4f318a44d8ba5a13161a541db6
Fredrik Bergqvist <fredrik@bergqvist.it>
2025-01-04 18:28:35 +0100

    see if I can get d.ts to work

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+interface Window {
+	NidhuggModal: INidhuggModal;
+	NidhuggImageModal: INidhuggImageModal;
+}
+interface HTMLElementTagNameMap {
+	"nidhugg-modal": INidhuggModal;
+	"nidhugg-image-modal": INidhuggImageModal;
+}
