f3c11f99a6b270216a39a37f64f1eee8d4300eda
Fredrik Bergqvist <fredrik@bergqvist.it>
2025-01-05 21:54:41 +0100

    Wip: back to top component

---
+++ content/components/back-top-top.md
@@ -0,0 +1,19 @@
+---
+title: Back-to-top button
+description: Wrap around the main part of your page to add a back-to-top button.
+image: ../public/img/components/imageModal/image-modal.jpeg
+date: 2025-01-05
+---
+
+Wrap around the main part of your page to add a back-to-top button.
+
+## Usage
+
+```html
+
+<nidhugg-back-to-top>
+	<div style="height:400vh">
+		<p>Lots of great content...</p>
+	</div>
+</nidhugg-back-to-top>
+```
---
+++ public/script/nidhugg/backToTop/nidhuggBackToTop.ts
@@ -0,0 +1,69 @@
+class NidhuggBackToTop extends HTMLElement {
+	static observedAttributes = ["scroll-id"];
+	#anchorId = "nidhugg-back-to-top";
+	static {
+		customElements.define("nidhugg-back-to-top", this);
+	}
+	#anchorClass = "nidhugg-back-to-top__anchor";
+	#scrollToId = "nidhugg-scroll-to";
+	#endOfContentId = "nidhugg-content-end";
+	#supportsScrollAnimation = CSS.supports("animation-timeline: scroll()");
+
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		if (!this.getAttribute("scroll-id") || !this.#supportsScrollAnimation) {
+			const scrollToEl = document.createElement("span");
+			scrollToEl.id = this.#scrollToId;
+			this.prepend(scrollToEl);
+		}
+		if (!this.#supportsScrollAnimation) {
+			const contentEndEl = document.createElement("span");
+			contentEndEl.id = this.#endOfContentId;
+			this.appendChild(contentEndEl);
+		}
+		const scrollToId = this.getAttribute("scroll-id") || this.#scrollToId;
+		const anchor = document.createElement("a");
+		anchor.innerHTML = `
+			<figure>
+				<svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24"><path d="M23.71,16.29,15.54,8.12a5,5,0,0,0-7.08,0L.29,16.29a1,1,0,0,0,1.42,1.42L9.88,9.54a3,3,0,0,1,4.24,0l8.17,8.17a1,1,0,0,0,1.42,0A1,1,0,0,0,23.71,16.29Z"/></svg>
+				<figcaption>Back to top</figcaption>
+			</figure>`;
+
+		anchor.href = `#${scrollToId}`;
+		anchor.id = this.#anchorId;
+		anchor.classList.add(this.#anchorClass);
+		if (this.#supportsScrollAnimation) {
+			anchor.classList.add("nidhugg-scroll-animation");
+		}
+		document.body.appendChild(anchor);
+
+		if (!this.#supportsScrollAnimation) {
+			const topScrollEl = document.getElementById(this.#scrollToId);
+			if (!topScrollEl) {
+				return;
+			}
+			const browserHeight = window.innerHeight;
+			const observer = new IntersectionObserver(
+				(entries) => {
+					entries.forEach((entry) => {
+						if (!entry.isIntersecting) {
+							anchor.style.opacity = "1";
+						} else {
+							anchor.style.opacity = "0";
+						}
+					});
+				},
+				{
+					root: null,
+					rootMargin: `${browserHeight}px`,
+					threshold: 1,
+				}
+			);
+
+			observer.observe(topScrollEl);
+		}
+	}
+}
